338ee1e5ef6601b787faf39c2daedc899ecdc8fe
xiaomo <1993204262@qq.com>
2024-02-20 22:50:00 +0800

    useState 设置值为一个函数，而不是去调用它

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ function App() {
   // react只在初次渲染时保存初始状态,后续渲染时会将其忽略
   // 注意:每次渲染还是会调用此函数,只不过后续渲染会忽略函数的返回结果
   // 可以通过传递函数本身来解决,react仅会在初次调用函数
-  const [count, setCount] = useState(getCount);
+  const [count, setCount] = useState(getCount());
 
   return (
     <div className="App">
       {count}
-      <button onClick={() => setCount(count + 1)}>add</button>
+      {/* 尝试将state设置为一个函数,但是函数却被调用了,因为react将getCount当作是一个更新函数 */}
+      <button onClick={() => setCount(getCount)}>add</button>
     </div>
   );
 }
