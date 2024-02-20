2dffc45631d350509d3636b6ffcac9325cd9620b
xiaomo <1993204262@qq.com>
2024-02-20 22:37:24 +0800

    useState 初始值为函数时，避免重复调用初始函数

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function getCount() {
+  console.log('init');
+  return 0;
+}
 
 function App() {
+  // react只在初次渲染时保存初始状态,后续渲染时会将其忽略
+  // 注意:每次渲染还是会调用此函数,只不过后续渲染会忽略函数的返回结果
+  // 可以通过传递函数本身来解决,react仅会在初次调用函数
+  const [count, setCount] = useState(getCount);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {count}
+      <button onClick={() => setCount(count + 1)}>add</button>
     </div>
   );
 }
