aa989b8541482139ab595de9a35f926b61279d53
xiaomo <1993204262@qq.com>
2024-02-26 11:07:03 +0800

    useRef属性为一个函数且在函数里面调用setState时，注意！！！

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import { createRef, useState } from 'react';
-import Providers from './components/Providers';
-import Container from './components/Container';
+import { createRef, useRef, useState } from 'react';
 import MyInput from './components/MyInput';
 
 function App() {
-    const [theme, setTheme] = useState('light');
     const [count, setCount] = useState(0);
 
     // 创建一个ref对象,每次渲染都是同一个对象引用
     const inpRef = createRef();
 
+    // 创建一个ref对象
+    const countRef = useRef(0);
+
+    const timer = useRef(null);
+
+    const handleStop = () => {
+        if (!timer.current) return;
+        clearInterval(timer.current);
+        timer.current = null;
+    };
+
+    const handleStart = () => {
+        handleStop();
+        // 这里count一直为之前的值,是因为timer.current不会因为重新渲染而发生变化,还是过去的引用,所以取的count也是过去的值
+        timer.current = setInterval(() => {
+            setCount(count + 1);
+        }, 1000);
+    };
+
     return (
         <div className='App' style={{ width: 300 }}>
-            <Providers theme={theme}>
-                <Container theme={theme} setTheme={setTheme} />
-            </Providers>
-            <div>
-                {count}
-                <button onClick={() => setCount(count + 1)}>add</button>
-            </div>
-
             <MyInput ref={inpRef} />
             <button
                 onClick={() => {
@@ -28,6 +36,25 @@ function App() {
             >
                 聚焦
             </button>
+
+            <div>
+                <span>{countRef.current}</span>
+                <button
+                    onClick={() => {
+                        // 修改ref的值不会触发react重新渲染
+                        countRef.current = countRef.current + 1;
+                        console.log(countRef.current);
+                    }}
+                >
+                    +1
+                </button>
+            </div>
+
+            <div>
+                {count}
+                <button onClick={handleStart}>开始</button>
+                <button onClick={handleStop}>暂停</button>
+            </div>
         </div>
     );
 }
