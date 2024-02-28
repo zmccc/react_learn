34f87e7740e8a259357a3eba443c429ae4620e47
xiaomo <1993204262@qq.com>
2024-02-28 15:52:32 +0800

    useEffect 通过props或state来更新state（不建议）

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import { useState } from 'react';
-import PlayGround from './components/PlayGround';
 
 function App() {
-    const [show, setShow] = useState(false);
+    const [firstName, setFirstName] = useState('hello');
+    const [lastName, setLastName] = useState('world');
+
+    const fullName = firstName + ' ' + lastName;
+
+    // useEffect会在组件渲染到屏幕上时调用
+    // 由于firstName改变触发重新渲染,等firstName改完在运行useEffect内到代码,导致再次触发一次页面渲染
+    // 不建议通过useEffect监听一些state来更新另外的state
+    console.log('改变' + fullName);
+    // useEffect(() => {
+    //     setFullName(firstName + ' ' + lastName);
+    // }, [firstName, lastName]);
+
     return (
         <div>
-            <button
-                onClick={() => {
-                    setShow(!show);
-                }}
-            >
-                {show ? '卸载' : '挂载'}
-            </button>
-            {show && <PlayGround />}
+            <div>
+                <span>first:</span>
+                <input
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div>
+                <span>last:</span>
+                <input
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <span>{fullName}</span>
         </div>
     );
 }
